6b24dcc1246e60ce7f5a5bea472e9154e3e0be7f
Matthew Brennecke <mpbrenn@umich.edu>
2019-11-05 12:57:57 -0500

    final test with reduced coverage

--- test_rpn.py
+++ test_rpn.py
@@ -8,13 +8,13 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
-    def test_sub(self):
+    #def test_sub(self):
         result = rpn.calculate('5 3 -')
         self.assertEqual(2, result)
-    def test_mul(self):
+    #def test_mul(self):
         result = rpn.calculate('12 8 *')
         self.assertEqual(96, result)
-    def test_div1(self):
+    #def test_div1(self):
         result = rpn.calculate('12 8 /')
         self.assertEqual(1, result)
     def test_div2(self):
